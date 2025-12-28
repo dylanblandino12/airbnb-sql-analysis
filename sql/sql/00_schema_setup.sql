bc1d660ee8fc0bdecf89f9c518edff89de34fe98
dylanblandino12 <dylanbu0908@gmail.com>
2025-12-27 22:22:59 -0600

    Create 00_schema_setup.sql

---
+++ sql/sql/00_schema_setup.sql
@@ -0,0 +1,7 @@
+-- =====================================
+-- Schema setup and data preparation
+-- =====================================
+
+-- Define primary key for listings
+ALTER TABLE listings
+ADD PRIMARY KEY (id);
