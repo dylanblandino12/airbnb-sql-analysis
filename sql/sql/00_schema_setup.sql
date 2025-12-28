b0dda1223cd051998e1455e416980508f5c65918
dylanblandino12 <dylanbu0908@gmail.com>
2025-12-27 22:23:38 -0600

    Delete sql/sql/00_schema_setup.sql

--- sql/sql/00_schema_setup.sql
+++
@@ -1,7 +0,0 @@
--- =====================================
--- Schema setup and data preparation
--- =====================================
-
--- Define primary key for listings
-ALTER TABLE listings
-ADD PRIMARY KEY (id);
