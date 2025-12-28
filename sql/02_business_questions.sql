1984899e229862a4eb47348f8868767e4af2f72d
dylanblandino12 <dylanbu0908@gmail.com>
2025-12-27 22:50:31 -0600

    Create 02_business_questions.sql

---
+++ sql/02_business_questions.sql
@@ -0,0 +1,6 @@
+-- =====================================
+-- File: 02_business_questions.sql
+-- Purpose: Answer key business questions using SQL
+-- Dataset: Airbnb (Denver)
+-- =====================================
+
